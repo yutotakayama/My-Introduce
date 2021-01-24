5b607bca321417b336177b56aff6d51fcc3fa77c
yutotakayama <71390128+yutotakayama@users.noreply.github.com>
2021-01-24 23:36:45 +0900

    Add files via upload

--- js/about.js
+++ js/about.js
@@ -1,49 +1,7 @@
 'use strict';
 
 {
-  // メニュー
-  menu();
-  function menu() {
-    const open = document.getElementById('open');
-    const overContent = document.querySelector('.over-content');
-    const close = document.getElementById('close');
   
-    // ハンバーガーアイコンをクリックでメニューを表示
-    open.addEventListener('click', () => {
-      overContent.classList.add('show');
-      open.classList.add('hide');
-    });
-  
-    // ✖をクリックでメニューを非表示
-    close.addEventListener('click', () => {
-      overContent.classList.remove('show');
-      open.classList.remove('hide');
-    });
-
-  }
-
-  // ページトップへ戻る
-  scrollTop('reply', 500);
-
-  function scrollTop(elem,duration) {
-    let target = document.getElementById(elem);
-    target.addEventListener('click', function() {
-      let currentY = window.pageYOffset; 
-      let step = duration/currentY > 1 ? 10 : 100;
-      let timeStep = duration/currentY * step;
-      let intervalID = setInterval(scrollUp, timeStep);
-
-      function scrollUp(){
-        currentY = window.pageYOffset;
-        if(currentY === 0) {
-            clearInterval(intervalID);
-        } else {
-            scrollBy( 0, -step );
-        }
-      }
-    });
-  }
-
   // アニメーション about
   window.addEventListener('load', about, false);
   function about() {
@@ -100,5 +58,60 @@
       });
   }
 
+    // スライダー
+    document.addEventListener('DOMContentLoaded', () => {
+      const slide = new Slider('.swiper-container');
+    });
+
+    class Slider {
+      constructor(el) {
+        this.el = el;
+        this.swiper = this._Swiper();
+      }
+
+      _Swiper() {
+        return new Swiper(this.el, {
+          loop: true,
+          grabCursor: true,
+          effect: 'coverflow',
+          centeredSlides: true,
+          slidesPerView: 1,
+          speed: 1000,
+          breakpoints: {
+            560: {
+              slidesPerView: 2,
+            }
+          },
+          autoplay: {
+            delay: 1000,
+            disableOnInteraction: false
+          }
+        });
+      }
+    }
+ 
+
+    // ページトップへ戻る
+
+    scrollTop('reply', 500);
+
+    function scrollTop(elem,duration) {
+      let target = document.getElementById(elem);
+      target.addEventListener('click', function() {
+        let currentY = window.pageYOffset; 
+        let step = duration/currentY > 1 ? 10 : 100;
+        let timeStep = duration/currentY * step;
+        let intervalID = setInterval(scrollUp, timeStep);
+
+        function scrollUp(){
+          currentY = window.pageYOffset;
+          if(currentY === 0) {
+              clearInterval(intervalID);
+          } else {
+              scrollBy( 0, -step );
+          }
+        }
+      });
+    }
 
 }
--- js/contact.js
+++ js/contact.js
@@ -1,25 +1,6 @@
 'use strict';
 
 {
-  menu();
-  function menu() {
-    const open = document.getElementById('open');
-    const overContent = document.querySelector('.over-content');
-    const close = document.getElementById('close');
-  
-    // ハンバーガーアイコンをクリックでメニューを表示
-    open.addEventListener('click', () => {
-      overContent.classList.add('show');
-      open.classList.add('hide');
-    });
-  
-    // ✖をクリックでメニューを非表示
-    close.addEventListener('click', () => {
-      overContent.classList.remove('show');
-      open.classList.remove('hide');
-    });
-  }
-
     window.addEventListener('load', me, false);
     function me() {
       var textWrapper = document.querySelector('#me');
