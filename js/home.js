ca1c749fcb2b529cb88d509f419082c4fb92c2cc
Yuto Takayama <71390128+yutotakayama@users.noreply.github.com>
2021-02-03 16:25:44 +0900

    Add files via upload

--- js/home.js
+++ js/home.js
@@ -1,31 +1,6 @@
 'use strict';
 
 {
-  // スライドショー
-  window.addEventListener('load', function () {
-    viewSlide('.slide img');
-  });
-
-  function viewSlide(className, slideNo = -1) {
-    let imgArray = document.querySelectorAll(className);
-
-    if (slideNo >= 0) {
-      //初回以外は現在のスライドを消す
-      imgArray[slideNo].style.opacity = 0;
-    }
-
-    slideNo++;
-
-    if (slideNo >= imgArray.length) {
-      slideNo = 0; //次のスライドがなければ最初のスライドへ戻る
-    }
-
-    imgArray[slideNo].style.opacity = 1;
-
-    setTimeout(function(){viewSlide(className, slideNo);}, 3000);
-  }
-
-
   // アニメーション about
   window.addEventListener('load', about, false);
   function about() {
